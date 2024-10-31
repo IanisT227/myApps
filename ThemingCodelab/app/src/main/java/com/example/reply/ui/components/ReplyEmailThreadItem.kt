add1ea4b9afc100c28b37db1ac6067ce42981d97
Ianis_Teja <ianist2001@gmail.com>
2024-10-31 16:23:25 +0200

    Optimized imports

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailThreadItem.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailThreadItem.kt
@@ -27,7 +27,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarBorder
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
